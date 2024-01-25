07384e4262dc3458b89fa5220f252cd7fd111f23
SPIDEYcharles <90609397+SPIDEYcharles@users.noreply.github.com>
2024-01-25 13:19:53 +0000

    abhinav babai lappy

---
+++ Patterns/halfpyramid.java
@@ -0,0 +1,11 @@
+public class halfpyramid {
+    public static void main(String[] args) {
+        int s =4;
+        for (int line = 1; line <= s; line++) {
+            for (int i = 1; i <=line; i++) {
+                System.out.print(i+"  ");
+            }
+            System.out.println("");
+        }
+    }
+}
