368f8d9c67e4e14a9dcb7ce055af79ccf5fe9ca9
SPIDEYcharles <90609397+SPIDEYcharles@users.noreply.github.com>
2024-04-22 10:17:52 +0530

    microsoft lzppy

--- Binary/compliment.java
+++ Binary/compliment.java
@@ -1,5 +1,5 @@
 public class compliment{
     public static void main(String add[]){
-        
+        System.out.println(~1);
     }
 }
---
+++ Binary/fastExpo.java
@@ -0,0 +1,5 @@
+public class fastExpo {
+    public static void main(String[] args) {
+        
+    }
+}
